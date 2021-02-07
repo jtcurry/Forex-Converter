d2be91f0336ad51917cd4bccfa81a7c2cc28f6dd
John Curry <jtc3852@gmail.com>
2021-02-07 18:11:37 -0500

    Add currency codes

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 from flask import Flask, render_template, request, flash, redirect
 from forex_python.converter import CurrencyRates, CurrencyCodes, RatesNotAvailableError
 from decimal import Decimal, InvalidOperation
+from helpers import *
 
 app = Flask(__name__)
 c = CurrencyRates()
 code = CurrencyCodes()
 app.config['SECRET_KEY'] = "newKey"
 
+codes = sort_dict(currency_code_dict)
+
+
 @app.route("/")
 def show_home():
-  return render_template("index.html")
+    return render_template("index.html", codes=codes)
+
 
 @app.route("/convert")
 def convert():
     convert_from = request.args["from"]
     convert_to = request.args["to"]
-    convert_amount = request.args["amount"] 
-    get_converted = c.convert(convert_from, convert_to, Decimal(convert_amount))
-    converted = round(get_converted,2)
+    convert_amount = request.args["amount"]
+    get_converted = c.convert(convert_from, convert_to,
+                              Decimal(convert_amount))
+    converted = round(get_converted, 2)
     from_symbol = code.get_symbol(convert_from)
     to_symbol = code.get_symbol(convert_to)
     rates = c.get_rates(convert_from)
-    return render_template("conversion.html", convert_from = convert_from, convert_to = convert_to, convert_amount = convert_amount, converted = converted, from_symbol = from_symbol, to_symbol= to_symbol, rates = rates)
+    return render_template("conversion.html",
+                           convert_from=convert_from,
+                           convert_to=convert_to,
+                           convert_amount=convert_amount,
+                           converted=converted,
+                           from_symbol=from_symbol,
+                           to_symbol=to_symbol,
+                           rates=rates,
+                           codes=codes)
---
+++ helpers.py
@@ -0,0 +1,41 @@
+currency_code_dict = {
+    "EUR": "Euro Member Countries",
+    "IDR": "Indonesia Rupiah",
+    "BGN": "Bulgaria Lev",
+    "ILS": "Israel Shekel",
+    "GBP": "United Kingdom Pound",
+    "DKK": "Denmark Krone",
+    "CAD": "Canada Dollar",
+    "JPY": "Japan Yen",
+    "HUF": "Hungary Forint",
+    "RON": "Romania New Leu",
+    "MYR": "Malaysia Ringgit",
+    "SEK": "Sweden Krona",
+    "SGD": "Singapore Dollar",
+    "HKD": "Hong Kong Dollar",
+    "AUD": "Australia Dollar",
+    "CHF": "Switzerland Franc",
+    "KRW": "Korea (South) Won",
+    "CNY": "China Yuan Renminbi",
+    "TRY": "Turkey Lira",
+    "HRK": "Croatia Kuna",
+    "NZD": "New Zealand Dollar",
+    "THB": "Thailand Baht",
+    "USD": "United States Dollar",
+    "NOK": "Norway Krone",
+    "RUB": "Russia Ruble",
+    "INR": "India Rupee",
+    "MXN": "Mexico Peso",
+    "CZK": "Czech Republic Koruna",
+    "BRL": "Brazil Real",
+    "PLN": "Poland Zloty",
+    "PHP": "Philippines Peso",
+    "ZAR": "South Africa Rand"
+}
+
+
+def sort_dict(test_dict):
+    sorted_dict = {}
+    for k, v in sorted(test_dict.items()):
+        sorted_dict[k] = v
+    return sorted_dict
